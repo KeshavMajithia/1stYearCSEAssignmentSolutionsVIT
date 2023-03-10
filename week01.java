64fcf179e22a046a78f4431c760c2b07ba00d500
KeshavMajithia <65853673+KeshavMajithia@users.noreply.github.com>
2023-03-10 09:38:10 +0530

    Update week01.java

--- week01.java
+++ week01.java
@@ -13,5 +13,9 @@ public static void main(String[] args){
     Student s=new Student();
     System.out.println("\nDefault Constructor values: \n");
     System.out.println("Student id: :"+s.id+"\nStudent Name: "+s.name);
+    
+    System.out.println("\nParameterized Constructor Values: \n");
+    Student student=new Student(10, "David");
+    System.out.println("Student id: "+student.id+"\nStudent Name: "+student.name);
 }
 }
