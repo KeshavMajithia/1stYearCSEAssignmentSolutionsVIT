91164c4353ac1e469a0c186f7e7c0009198edd69
KeshavMajithia <65853673+KeshavMajithia@users.noreply.github.com>
2023-03-13 10:02:35 +0530

    Update week02.java

--- week02.java
+++ week02.java
@@ -18,3 +18,20 @@ class Main{
         halogen.turnOff();
     }
 }
+
+
+//insert question here
+class Adder{
+    static int add(int a, int b){
+        return a+b;
+    }
+    static int add(int a, int b, int c){
+        return a+b+c;
+    }
+}
+class TestOverLoading1{
+    public static void main(String[]args){
+        System.out.println(Adder.add(11,11));
+        System.out.println(Adder.add(11,11,11));
+    }
+}
