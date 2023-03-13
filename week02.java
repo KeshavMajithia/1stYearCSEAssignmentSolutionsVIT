ad7331b85aa2c088c622e7c33d5bb1f9b5e477e9
KeshavMajithia <65853673+KeshavMajithia@users.noreply.github.com>
2023-03-13 10:32:40 +0530

    Update week02.java

--- week02.java
+++ week02.java
@@ -35,3 +35,31 @@ class TestOverLoading1{
         System.out.println(Adder.add(11,11,11));
     }
 }
+
+//write a java program to implement array of objects 
+public class ArrayOfObjects{
+    public static void main(String[]args)
+{
+Product[] obj = new Product[5];
+obj[0]=new Product(23907,"Dell Laptop");
+obj[1]=new Product(91340,"HP 630");
+obj[2]=new Product(29823,"LG OLED TV");
+obj[3]=new Product(11908,"iQOO 9 5G");
+obj[4]=new Product(43590,"Asus ROG");
+System.out.println("Product Object 1: ");
+obj[0].display();
+    }
+}
+class Product{
+    int pro_Id;
+    String pro_Name;
+    Product(int pid, String n)
+{
+    pro_Id=pid;
+    pro_Name=n;
+}
+public void display()
+{
+    System.out.println("Product id: "+pro_Id+" "+"Product Name: "+pro_Name);
+}
+}
